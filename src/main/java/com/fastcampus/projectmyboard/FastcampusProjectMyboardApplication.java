7eab98c7347065531e589cc5ff008bb8b5a4090e
haribo033 <ehfaksdl033@gmail.com>
2023-04-08 02:04:20 +0900

    firstCommitTest

---
+++ src/main/java/com/fastcampus/projectmyboard/FastcampusProjectMyboardApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.projectmyboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastcampusProjectMyboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastcampusProjectMyboardApplication.class, args);
+    }
+
+}
