375a3efd282ba737dade4dd0edfbda167d17953d
haribo033 <ehfaksdl033@gmail.com>
2023-04-09 20:49:29 +0900

    env: update - Repository에 Rest기능 추가

--- src/main/java/com/fastcampus/projectmyboard/repository/ArticleCommentRepository.java
+++ src/main/java/com/fastcampus/projectmyboard/repository/ArticleCommentRepository.java
@@ -2,6 +2,8 @@ package com.fastcampus.projectmyboard.repository;
 
 import com.fastcampus.projectmyboard.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource //spring data rest 사용 할 준비
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
