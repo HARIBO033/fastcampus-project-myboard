367034a309520e135cfaf41b8c4b04a0a257ffc4
haribo033 <ehfaksdl033@gmail.com>
2023-04-11 06:34:35 +0900

    env: set - 게시글 관련 뷰테스트 작성

---
+++ src/main/java/com/fastcampus/projectmyboard/controller/ArticleController.java
@@ -0,0 +1,12 @@
+package com.fastcampus.projectmyboard.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RequestMapping("/articles")
+public class ArticleController {
+    //controllertest 만들어야됨
+}
