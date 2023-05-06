0b9889bb8c5eaf520da9936773268f80cb5f469c
haribo033 <ehfaksdl033@gmail.com>
2023-05-06 17:56:51 +0900

    env - set 시큐리티 설정과 자바 대응 코드 구현
    스프링 부트 버전 2.7 이후 가이드에 맞춰 시큐리티 설정 적용, 징증 정보를 db로부터 부르는 빈과 패스워드 인코더 빈까지 등록

--- src/main/java/com/fastcampus/projectmyboard/config/JpaConfig.java
+++ src/main/java/com/fastcampus/projectmyboard/config/JpaConfig.java
@@ -1,10 +1,14 @@
 package com.fastcampus.projectmyboard.config;
 
 
+import com.fastcampus.projectmyboard.dto.security.BoardPrincipal;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Optional;
 
@@ -12,8 +16,13 @@ import java.util.Optional;
 @EnableJpaAuditing
 public class JpaConfig {
 
-    @Bean
-    public AuditorAware<String> auditorAware() {
-        return () -> Optional.of("haribo"); // TODO : 스프링 시큐리티로 인증 기능을 붙이게 될 때, 수정
-    }
+  @Bean
+  public AuditorAware<String> auditorAware() {
+    return () -> Optional.ofNullable(SecurityContextHolder.getContext())// 시큐리티 context를 모두 가져옴
+            .map(SecurityContext::getAuthentication)// SequrityContext에 Authentication을 가져옴
+            .filter(Authentication::isAuthenticated)//Authentication의 로그인상태 확인
+            .map(Authentication::getPrincipal)//로그인 정보인 Principal 꺼내옴
+            .map(BoardPrincipal.class::cast)//BoardPrincipal을 불러와서 타입캐스팅을 함
+            .map(BoardPrincipal::getUsername);//getUsername으로 실제 유저정보를 불러옴
+  }
 }
